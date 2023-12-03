d2e41b503d74d95b602a4c9e2aa6bfe6fb331be9
tasnyza <117885120+tasnyza@users.noreply.github.com>
2023-12-03 10:20:56 -0500

    json changes

--- Projects/json/script.js
+++ Projects/json/script.js
@@ -19,12 +19,12 @@
 //         })
 // })
 
-$(function () {
-    // place kitten
-    fetch("https://placekitten.com/")
-        .then(function (response) {
-            return response.json();
-        })
+// $(function () {
+//     // place kitten
+//     fetch("https://placekitten.com/")
+//         .then(function (response) {
+//             return response.json();
+//         })
         // .then(function (data) {
         //     console.log(data.data);
         //     console.log(data.data[0].color.h);
@@ -34,10 +34,10 @@ $(function () {
         //     $("main").append(`<p>${data.data[0].color.l}<p>`)
         //     $("main").append(`<p>${data.data[0].color.s}<p>`)
         // })
-        .catch(function (error) {
-            $("main").html("There's been an error!");
-        })
-})
+//         .catch(function (error) {
+//             $("main").html("There's been an error!");
+//         })
+// })
 
 
 // let chips = {
@@ -65,3 +65,45 @@ $(function () {
 //     $("main").append("<h2>" + chips.name + "</h2>")
 //     // $("main").append('<div><img src="Github/Projects/json/${chips.photo}" width="50%"><h2>${chips.name}</h2>'); 
 // })
+
+
+
+// took help from https://www.youtube.com/watch?v=uxf0--uiX0I&ab_channel=TheCodingTrain
+
+
+const api_url = 'https://api.wheretheiss.at/v1/satellites/25544'; // making the api link into a constant
+async function getISS() { // fetching the data
+    const response = await fetch(api_url);
+    const data = await response.json(); //turning the data into json
+    // console.log(data);
+    const { latitude, longitude }  = data;
+    console.log(latitude);
+    console.log(longitude);
+
+    document.getElementById('lat').textContent = latitude;
+    document.getElementById('long').textContent = longitude;
+
+    console.log(latitude);
+    console.log(longtitude);
+}
+
+getISS();
+
+// const apiUrl = "https://random-d.uk/api/v2";
+
+// function getRandomDuck() {
+//     // Make a request to the /random endpoint
+//     fetch(`${apiUrl}/random?type=JPG`)
+//         .then(response => response.json())
+//         .then(data => {
+//             // Update the image source with the random duck image URL
+//             document.getElementById("duckImage").src = data.url;
+//         })
+//         .catch(error => {
+//             console.error("Error fetching random duck:", error);
+//         });
+// }
+
+// function getCatFact() {
+//     const api_url2 = 
+// }
